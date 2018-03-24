28c6540ef30516890c383c25b1fb32e37494bed3
Emanuel M <emanuel.mares@gmail.com>
2018-03-25 00:30:47 +0200

    Added the function to generate the random paterns for the board

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
-let icons = ["code-branch", "angular", "docker", "google", "git", "medium", "php", "react", "code", "telegram", "vuejs", "wordpress", "android"];
+const icons = ["cogs", "chrome", "apple", "google", "git", "medium", "firefox", "cubes", "code", "edge", "envira", "wordpress", "android"];
+function generateRandom(length, maxValue){
+    result = [];
+    for(let i=0;i<length;i++){
+        let randomNumber = Math.floor((Math.random() * maxValue));
+        while(result.includes(randomNumber)){
+            randomNumber = Math.floor((Math.random() * maxValue));
+        }
+        result[i] = randomNumber;
+    }
+    return result;
+}
+const generateBoard = function(){
+    let iconSet = generateRandom(8,12);
+    let positions = generateRandom(16,16);
+    let newArray = [];
+    for(let i=0;i<positions.length;i++){
+        newArray.push([positions[i],iconSet[Math.floor(i/2)]]);
+    };
+    console.log(newArray);
+    let cards = document.getElementsByClassName("card");
+    newArray.forEach(function(el){
+        cards[el[0]].firstChild.classList.add("fa-"+icons[el[1]]);
+    });
+}();
